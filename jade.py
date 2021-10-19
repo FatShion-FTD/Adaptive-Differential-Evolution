685624ffcc86ff039eb539b74cc7096fdf2a2d99
FatShion-FTD <48754270+FatShion-FTD@users.noreply.github.com>
2021-10-19 02:19:27 +0200

    Update and rename assignment2_jade.py to jade.py

--- jade.py
+++ jade.py
@@ -1,12 +1,7 @@
-import sys
-sys.path.insert(0, 'evoman')
-
 import os
 import glob
 from math import fabs, sqrt
 import time
-from demo_controller import player_controller
-from evoman.environment import Environment
 import numpy as np
 from halton import halton
 import random
@@ -110,7 +105,7 @@ def jade(thetas_limit, target_vectors_init, uCR=0.5, uF=0.6, c=0.1, NP=10, max_g
         prev_best = target_fitness[best]
         
         if notimprov >= 5:
-            # cheating
+            # halton shaking
             for i in range(0,5): # repeat 5 times
                 rand_vector_index = np.random.randint(low=0, high=NP)
                 # cannot touch best
@@ -124,112 +119,21 @@ def jade(thetas_limit, target_vectors_init, uCR=0.5, uF=0.6, c=0.1, NP=10, max_g
 
 # runs simulation
 def simulation(x):
-    f, p, e, t = env.play(pcont=x)
-    return f
+    # your simulation function for each indivudal
+    return 0
 
 
-# evaluation
-def evaluate(x):
-    return np.array(list(map(lambda y: simulation(y), x)))
 
+experiment_name = 'jade'          
+npop = 20   # population size
+gens = 150  # generations 
+n_vars = 20 # dimensions
 
-# choose this for not using visuals and thus making experiments faster
-headless = True
-if headless:
-    os.environ["SDL_VIDEODRIVER"] = "dummy"
+ini = time.time()  # sets time marker
 
-               
-experiment_name_list = ['assignment2_jade_1_group2', 'assignment2_jade_2_group2','assignment2_jade_3_group2',
-'assignment2_jade_4_group2','assignment2_jade_5_group2','assignment2_jade_6_group2','assignment2_jade_7_group2'
-'assignment2_jade_8_group2','assignment2_jade_9_group2','assignment2_jade_10_group2']
+# evolution
+jade(n_vars, pop, NP=npop, experiment_name=experiment_name,max_gen=gens)
 
-n_hidden_neurons = 10
+fim = time.time()  # prints total execution time for experiment
 
-dom_u = 1
-dom_l = -1
-npop = 20   # Can be modified
-gens = 150   # Can be modified
-# mutation = 0.2
-# last_best = 0
-
-
-for experiment_name_index in experiment_name_list:
-    enemies_list = [1,2,3,4,5,6,7,8]
-    # enemies_list = [2,5,7,8]
-
-    experiment_name = experiment_name_index
-
-    if not os.path.exists(experiment_name):
-        os.makedirs(experiment_name)
-
-    # initializes simulation in individual evolution mode, for single static enemy.
-    env = Environment(experiment_name=experiment_name,
-                    enemies = enemies_list,
-                    multiplemode="yes",
-                    playermode="ai",
-                    randomini="yes",
-                    player_controller=player_controller(n_hidden_neurons),
-                    enemymode="static",
-                    level=2,
-                    speed="fastest")
-
-    n_vars = (env.get_num_sensors()+1) * n_hidden_neurons + (n_hidden_neurons+1)*5
-    # n_vars = env.get_num_sensors()
-
-    env.state_to_log()  # checks environment state
-
-    if not os.path.exists(experiment_name+'/evoman_solstate'):
-        print('\nNEW EVOLUTION\n')
-
-        pop = np.random.uniform(dom_l, dom_u, (npop, n_vars))
-        fit_pop = evaluate(pop)
-        best = np.argmax(fit_pop)
-        mean = np.mean(fit_pop)
-        std = np.std(fit_pop)
-        ini_g = 0
-        solutions = [pop, fit_pop]
-        env.update_solutions(solutions)
-    else:
-        print('\nCONTINUING EVOLUTION\n')
-
-        env.load_state()
-
-        pop = env.solutions[0]
-        fit_pop = env.solutions[1]
-
-        best = np.argmax(fit_pop)
-        mean = np.mean(fit_pop)
-        std = np.std(fit_pop)
-
-        # finds last generation number
-        file_aux = open(experiment_name+'/gen.txt', 'r')
-        ini_g = int(file_aux.readline())
-        file_aux.close()
-
-    # saves results for first pop
-    file_aux = open(experiment_name+'/results.txt', 'a')
-    file_aux.write('\n\ngen best mean std')
-    print('\n GENERATION '+str(ini_g)+' ' +
-            str(round(fit_pop[best], 6))+' '+str(round(mean, 6))+' '+str(round(std, 6)))
-    file_aux.write('\n'+str(ini_g)+' '+str(round(fit_pop[best], 6))+' '+str(
-        round(mean, 6))+' '+str(round(std, 6)))
-    file_aux.close()
-
-
-    ini = time.time()  # sets time marker
-
-    # evolution
-    jade(n_vars, pop, NP=npop, experiment_name=experiment_name,max_gen=gens)
-
-    fim = time.time()  # prints total execution time for experiment
-
-    print('\nExecution time: '+str(round((fim-ini)/60))+' minutes \n')
-    file_aux = open(experiment_name+'/results.txt', 'a')
-    file_aux.write('\nExecution time: '+str(round((fim-ini)/60))+' minutes \n')
-    file_aux.close()
-
-    # saves control (simulation has ended) file for bash loop file
-    file = open(experiment_name+'/neuroended', 'w')
-    file.close()
-
-    env.state_to_log()  # checks environment state
+print('\nExecution time: '+str(round((fim-ini)/60))+' minutes \n')
